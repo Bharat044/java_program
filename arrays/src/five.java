6ea29a2e53e0cafc363a074f7ebdd777a3289207
Bharat <bharat18csu044@ncuindia.edu>
2019-01-14 23:53:43 -0800

    classprg

---
+++ arrays/src/five.java
@@ -0,0 +1,24 @@
+import java.uitil*;
+class five {
+	public static void main(String args[]) {
+	Scanner in=new Scanner(System.in);
+	int i;  //for loop
+	int j;
+	int k;
+	int num1; //for limit of loops
+	int num2;
+	int num3;
+	int a[] = int[10]; // for saving array
+	int b[] = int[10];
+	int c[] = int[10];
+	System.out.println("please enter the number of elements of array 1");
+	num1 = in.nextInt();
+	System.out.println("please enter the number of elements of array 2");
+	num2 = in.nextInt();
+	System.out.println("enter the elments of array 1");
+	
+
+		}
+
+}
+
